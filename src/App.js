4727eca7e1ef0d0f48a1658c1b0980933e0a415e
Vidhi <vidhi.goyal1812@gmail.com>
2024-10-25 13:39:24 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Nav from "../src/Component/Navbar/Nav";
 import {Route, Routes } from "react-router-dom";
@@ -6,12 +6,18 @@ import  Home  from "./Pages/Home/Home";
 import Cart  from "./Pages/Cart/Cart";
 import Plcordr from "./Pages/Placeorder/Plcordr"
 import  Footer from "../src/Component/Footer/Footer";
+import Login from "./Component/LoginPage/login";
 
 function App() {
+
+  const[showlogin, setShowlogin]=useState(false);
   return (
     <>
+
+    {showlogin?<Login/>:<></>}
     <div className="App">
-      <Nav/>
+
+      <Nav setShowlogin={setShowlogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
